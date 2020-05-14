05a7ea646f456f97b10188151bd959dd0085783f
joaaoeu <hello@joaao.eu>
2020-05-14 07:06:02 +0100

    Complete challange

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  const getRepositories = useCallback(async () => {
+    try {
+      const { data: repositoriesList } = await api.get("/repositories");
+      setRepositories(repositoriesList);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getRepositories();
+  }, [getRepositories]);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const { data: newRepo } = await api.post("/repositories", {
+        url: "https://github.com/joaaoeu/gostack11-desafio1-conceitos-nodejs",
+        title: "Desafio Node.js",
+        techs: ["JavaScript", "Node.js"],
+      });
+      setRepositories([...repositories, newRepo]);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+      setRepositories(repositories.filter((repo) => repo.id !== id));
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
-    <div>
+    <>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(({ id, title }) => (
+          <li key={id}>
+            {title}
+            <button onClick={() => handleRemoveRepository(id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    </>
   );
 }
 
